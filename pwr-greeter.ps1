a22e51c4c1cbab028cb4af9c3bd5bf50a8125a82
Rocket <adamrivard87@gmail.com>
2025-03-22 20:35:12 -0400

    s

--- pwr-greeter.ps1
+++ pwr-greeter.ps1
@@ -105,7 +105,7 @@
             <Button x:Name="TitusWinUtilButton" Grid.Column="0" Grid.Row="2" Content="💻 Titus WinUtil" Margin="10"/>
             <Button x:Name="ScriptBinButton" Grid.Column="1" Grid.Row="2" Content="🗑️ Script Bin" Margin="10"/>
             <Button x:Name="MembersOnlyButton" Grid.Column="0" Grid.Row="3" Content="🔒 Members Only" Margin="10"/>
-            <Button x:Name="PersisantWindowsButton" Grid.Column="1" Grid.Row="3" Content="🪟 Persistant Windows" Margin="10"/>
+            <Button x:Name="PersisantWindowsButton" Grid.Column="1" Grid.Row="3" Content="🪟 Persistant Windows" Margin="10" FontFamily="Segoe UI Emoji"/>
             <Button x:Name="ToggleThemeButton" Grid.Column="0" Grid.Row="4" Content="🌗 Toggle Dark/Light Mode" Margin="10" Grid.ColumnSpan="2"/>
         </Grid>
     </Grid>
