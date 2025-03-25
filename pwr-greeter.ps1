200f7fdb61354d09a5aba1803151a4c4bc1e573d
Rocket <adamrivard87@gmail.com>
2025-03-25 18:12:14 -0400

    s

--- pwr-greeter.ps1
+++ pwr-greeter.ps1
@@ -90,6 +90,7 @@ try {
           <Setter Property="Background" Value="#2B2B2B"/>
           <Setter Property="FontSize" Value="12"/> <!-- Match button font size -->
           <Setter Property="FontWeight" Value="Bold"/> <!-- Match button font weight -->
+          <Setter Property="BorderBrush" Value="Transparent"/> <!-- Remove border -->
           <Setter Property="BorderThickness" Value="0"/>
           <Setter Property="Padding" Value="5"/>
       </Style>
